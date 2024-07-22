2bf45c0c0d851fc8739f063a14ae0a7280391c6b
angularvandan <vandankumar98522@gmail.com>
2024-07-22 15:26:37 +0530

    some extra bug fixed

--- src/app/components/pages/user-profile/profile-details/profile-details.component.ts
+++ src/app/components/pages/user-profile/profile-details/profile-details.component.ts
@@ -110,16 +110,16 @@ export class ProfileDetailsComponent implements OnInit{
   }
   confirm1() {
     this.confirmationService.confirm({
-      message: 'Are you sure you want to delete the product details?',
+      message: 'Are you sure you want to logout?',
       accept: () => {
         this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted' });
         this.userService.isLogin(false);
-        this.router.navigate(['/login']);
+        this.router.navigate(['/home']);
       },
       reject: (type: any) => {
         switch (type) {
           case ConfirmEventType.REJECT:
-            this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected' });
+            // this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected' });
             break;
           case ConfirmEventType.CANCEL:
             this.messageService.add({ severity: 'warn', summary: 'Cancelled', detail: 'You have cancelled' });
